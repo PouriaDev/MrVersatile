10b2c6743746b0d750db7a07df9a9c368a213196
p00ria <amir1300sobatdar@gmail.com>
2016-11-03 20:35:14 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "279273406:AAHnckCT3rMcsU3SsLctK9yypGAGE83J2TU"
+bot_token = "Token"
 send_api = "https://api.telegram.org/bot"..bot_token
-bot_version = "2.0"
-sudo_id = 180910633
+bot_version = "1.0"
+sudo_id = 142141024
